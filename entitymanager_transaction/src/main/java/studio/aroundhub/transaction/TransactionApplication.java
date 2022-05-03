accb8f1afe54a7255c9120d1c7e32fd8dcae0de8
Flature <jungwoo5394@gmail.com>
2022-05-03 23:04:45 +0900

    [update] 트랜잭션 소개 프로젝트 초기화

---
+++ entitymanager_transaction/src/main/java/studio/aroundhub/transaction/TransactionApplication.java
@@ -0,0 +1,34 @@
+package studio.aroundhub.transaction;
+
+import studio.aroundhub.transaction.factory.CEntityManagerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TransactionApplication {
+
+    public static void main(String[] args) throws IOException {
+
+        CEntityManagerFactory.initialization();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.println("Input your Command // [command] [name]");
+            String commandLine = br.readLine();
+            String[] splitCommand = commandLine.split(" ");
+
+            // 별도 값 검증하는 로직은 추가하지 않음
+            if (splitCommand[0].equalsIgnoreCase("exit")) {
+                System.out.println("System closed");
+                break;
+
+            } else if (splitCommand[0].equalsIgnoreCase("originalProvider")) {
+
+            } else if (splitCommand[0].equalsIgnoreCase("provider")) {
+
+            }
+        }
+    }
+}
